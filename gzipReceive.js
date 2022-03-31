fddd7e443e14c8e8de9a7d584dae671ad5452148
liwenjiao <393787391@qq.com>
2022-03-31 17:12:22 +0800

    feat：新增流监听处理

--- gzipReceive.js
+++ gzipReceive.js
@@ -1,13 +1,19 @@
 const http = require("http");
 const fs = require("fs");
 const zlib = require("zlib");
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 const server = http.createServer((req, res) => {
   const filename = req.headers.filename;
   console.log("文件请求接收:" + filename);
   req
-  .pipe(crypto.createDecipheriv('aes-128-cbc', '1234567890123456', '1234567890123456')) // 解密 第一个参数为加密算法,第二个为加密密钥,第三个为初始向量  http://nodejs.cn/api/crypto.html#cryptocreatedecipherivalgorithm-key-iv-options
+    .pipe(
+      crypto.createDecipheriv(
+        "aes-128-cbc",
+        "1234567890123456",
+        "1234567890123456"
+      )
+    ) // 解密 第一个参数为加密算法,第二个为加密密钥,第三个为初始向量  http://nodejs.cn/api/crypto.html#cryptocreatedecipherivalgorithm-key-iv-options
     .pipe(zlib.createGunzip())
     .pipe(fs.createWriteStream(filename))
     .on("finish", () => {
@@ -22,6 +28,6 @@ server.listen(3000, () => console.log("服务监听3000端口!"));
 /**
  * 1. 首先启动服务器 node gzipReceive.js
  * 2. 运行命令发送指定文件到指定服务器 node gzipSend <path to file> localhost
- * 
+ *
  * 注意: 发送的时候顺序是压缩加密,那么接收的时候是解密解压.
  */
---
+++ readStdin.js
@@ -0,0 +1,16 @@
+/**
+ * readStdin.js 运行起来后,终端输入,就会监听到输入的新数据.并打印.
+ */
+
+process.stdin
+  .on("readable", () => {
+    // 当流有可用的数据或者已到达流的末尾时触发readable事件. http://nodejs.cn/api/stream.html#event-readable
+    let chunk;
+    console.log("有新的数据可用...");
+    while ((chunk = process.stdin.read())) {
+      process.stdout.write(`data: ${chunk.toString()}\n\n`);
+    }
+  })
+  .on("end", () => {
+    process.stdout.write("end");
+  });
